19e0c9c13a580be5b23e387fe1fe42f923ed2e0e
Ethan McTague <ethan@tague.me>
2019-09-22 21:26:12 -0400

    Add error handling to shader

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -4,17 +4,30 @@
 #include <fstream>
 
 Shader::Shader(const std::string& vertexPath, const std::string& fragmentPath) {
+    // Load and compile individual shaders
     auto vertexShader = loadShader(vertexPath, GL_VERTEX_SHADER);
     auto fragmentShader = loadShader(fragmentPath, GL_FRAGMENT_SHADER);
 
     program = glCreateProgram();
 
+    // Link shaders together to form a shader program
     glAttachShader(program, vertexShader);
     glAttachShader(program, fragmentShader);
     glLinkProgram(program);
     glUseProgram(program);
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
+
+    // Freak out if shader linking failed
+    int status;
+    glGetProgramiv(program, GL_LINK_STATUS, &status);
+    if (!status) {
+        char log[512];
+        glGetProgramInfoLog(program, 512, nullptr, log);
+        glDeleteProgram(program);
+        throw std::runtime_error("Compile error in shader program: " + std::string(log));
+    }
+
 }
 
 Shader::~Shader() {
@@ -23,13 +36,27 @@ Shader::~Shader() {
 
 unsigned Shader::loadShader(const std::string& path, unsigned type) {
     std::ifstream input(path);
+
+    if (!input.is_open()) throw std::runtime_error("Unable to open file: " + path);
+
+    // Load shader file source code
     std::string source((std::istreambuf_iterator<char>(input)), std::istreambuf_iterator<char>());
     const auto cSource = source.c_str();
     const int length = source.length();
 
+    // Create and compile the shader
     auto shader = glCreateShader(type);
     glShaderSource(shader, 1, &cSource, &length);
     glCompileShader(shader);
+
+    // Freak out if compilation failed
+    int status;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    if (!status) {
+        char log[512];
+        glGetShaderInfoLog(shader, 512, nullptr, log);
+        throw std::runtime_error("Compile error in " + path + ": " + std::string(log));
+    }
 
     return shader;
 }
